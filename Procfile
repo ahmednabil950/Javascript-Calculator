31987b0f65872faa5e2aa117e298be074ff6e515
Ahmed <ahmednabil950@bitbucket.org>
2018-04-23 22:57:07 +0200

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn js_calculator.wsgi --log-file -
+worker: python manage.py runworker -v2
---
+++ calculator/static/Scripts/JavaScript1.js
@@ -0,0 +1,37 @@
+﻿var value;
+var result;
+var flag = 0;
+function showValue(i)
+{
+    if (flag == 0)
+    {
+        clear();
+    }
+    if (i == 0) {
+        clear();
+    }
+    else if(i==16)
+    {
+        document.getElementById("screen").innerHTML = equal();
+        flag = 0;
+    }
+    else {
+        flag = 1;
+        value = document.getElementsByTagName("button")[i].value;
+        document.getElementById("screen").innerHTML = document.getElementById("screen").innerHTML + value;
+    }
+}
+
+function equal()
+{
+    result = eval(document.getElementById("screen").innerHTML);
+    return result;
+}
+
+function clear()
+{
+    document.getElementById("screen").innerHTML = null;
+    value = null;
+    result = null;
+    flag = 0;
+}
